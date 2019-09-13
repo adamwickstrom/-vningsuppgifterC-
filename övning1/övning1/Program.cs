c0e5b6261e2a9385c324d499eb91ca3110229a45
Adam Wickström <adwi02002@utb.vaxjo.se>
2019-09-13 09:39:22 +0200

    test

---
+++ övning1/övning1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace övning1111111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv ett tal heltal: ");
+            double tal1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Skriv ännu ett heltal");
+            double tal2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("talens summa är: ");
+            Console.WriteLine(tal1 + tal2);
+            Console.WriteLine("talens differens är: ");
+            Console.WriteLine(tal1 - tal2);
+            Console.WriteLine("talens produkt är: ");
+            Console.WriteLine(tal1 * tal2);
+            Console.WriteLine("talens kvot är: ");
+            Console.WriteLine(tal1 / tal2);
+            
+
+        }
+    }
+}
