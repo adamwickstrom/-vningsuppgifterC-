8140604bfeae417403928e2f7de7a666c2fe7cf0
Adam Wickström <adwi02001@utb.vaxjo.se>
2019-10-08 13:26:16 +0200

    la till avrundningen

--- arrayers/arrayers/Program.cs
+++ arrayers/arrayers/Program.cs
@@ -7,7 +7,7 @@ namespace arrayersovning2
         static void Main(string[] args)
         {
             double[] temperatur = { 4.5, 3.2, 6.7, 6.1, 2.1, 1.6, 2.9 };
-            Console.WriteLine("Medeltemperaturen: " + Medel(temperatur));
+            Console.WriteLine("Medeltemperaturen: " + Math.Round(Medel(temperatur),1));
 
         }
 
@@ -19,6 +19,7 @@ namespace arrayersovning2
             for ( int i = 0; i < temperatur.Length; i++)
                 summa += temperatur[i];
             return (summa / temperatur.Length);
+            
 
 
         }
