182494d7d959e7c18ac76161666a3d21deebf7d0
Elev <Elev@TEB127022>
2019-12-13 10:05:38 +0100

    c vbhnjkl

---
+++ Klasserovning/Klasserovning/Fordon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Klasserovning
+{
+    class Fordon
+    {
+        private string tillverkare;
+        private string modell;
+        private int årsmodell;
+        private int vikt;
+
+        public void setTillverkare(string t) { tillverkare = t; }
+
+        public void setModell(string m) { modell = m; }
+
+        public void setÅrsmodell(int å) { årsmodell = å; }
+
+        public void setVikt(int v) { vikt = v; }
+
+
+
+
+
+
+
+    }
+}
---
+++ Klasserovning/Klasserovning/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Klasserovning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fordon f = new Fordon();
+            f.setTillverkare
+        }
+    }
+}
